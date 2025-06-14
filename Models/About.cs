18aa05ce00c9bca877bfc0c0372d939242561a1a
Alexander Cahueñas <bryan.cahuenas@udla.edu.ec>
2025-06-14 17:01:14 -0500

    commit

--- Models/About.cs
+++ Models/About.cs
@@ -7,5 +7,5 @@ internal class About
     
     public string MoreInfoUrl => "https://github.com/Axelzu/TALLERCREACIONDEAPUNTES.git"; 
 
-    public string Message => "Hola,Mi nombre es Alexander Cahueñas, estudio en la udla. Esta app fue desarrollada como parte del curso de desarrollo móvil con NET MAUI. Me interesa la programación  y sobre todo la creacion de videojuegos.";
+    public string Message => "Hola,Mi nombre es Alexander Cahueñas, estudio en la udla. Desarrolle una app en la plataforma de visual estudio en NET MAUI. Me interesa la programación  y sobre todo la creacion de videojuegos.";
 }
